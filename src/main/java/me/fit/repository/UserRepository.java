d4c0dc6ec3d2ff0df3a314bdbc79d81da391820a
k0ntrol <88510311+k0ntrol@users.noreply.github.com>
2025-04-04 19:01:55 +0200

    Drugi domaci zadatak

--- src/main/java/me/fit/repository/UserRepository.java
+++ src/main/java/me/fit/repository/UserRepository.java
@@ -5,6 +5,11 @@ import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import me.fit.model.User;
+import me.fit.model.UserProgress;
+import java.util.HashSet;
+import java.util.List;
+import java.util.List;
+import java.util.Set;
 
 @Dependent
 public class UserRepository {
@@ -15,5 +20,19 @@ public class UserRepository {
     @Transactional
     public User addUser (User user) {
         return em.merge(user);
+    }
+
+    @Transactional
+    public List<User> getAllUsers() {
+        List<User> users = em.createNamedQuery(User.GET_ALL_USERS, User.class)
+                .getResultList();
+
+        for (User user : users) {
+            List<UserProgress> progressList = em.createNamedQuery(UserProgress.GET_PROGRESS_FOR_USER, UserProgress.class)
+                            .setParameter("userId", user.getId()).getResultList();
+            user.setProgressList(new HashSet<>(progressList));
+        }
+
+        return users;
     }
 }
